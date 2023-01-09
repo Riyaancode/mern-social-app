42cf56b015bc548f6637dc0e9c70bf09e0c4afec
Mohammad Riyan <mohammadriyan428@gmail.com>
2023-01-10 04:48:28 +0500

    Feed frontend

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -9,7 +9,7 @@ function Feed(props) {
 
     const authLocal = useAuth()
     const user = JSON.parse(localStorage.getItem("user"));
- 
+
 
     const { _id, firstName, lastName, email, dateofBirth, location, occupation } = user;
 
@@ -94,16 +94,16 @@ function Feed(props) {
     const [friends, setFriends] = useState([])
     const fetchFriends = async () => {
 
-        
+
         axios.get(`http://localhost:4000/friendrequest/friends/${_id}`)
-        .then(response => {
-        
-            setFriends(response.data); 
-        })
-        .catch(error => {
-          console.log(error); // print the error
-        });
-      
+            .then(response => {
+
+                setFriends(response.data);
+            })
+            .catch(error => {
+                console.log(error); // print the error
+            });
+
     };
 
 
@@ -119,11 +119,11 @@ function Feed(props) {
                 .then(response => {
                     if (response.data.message) {
                         alert(response.data.message);
-                      } else {
+                    } else {
                         console.log(response.data.data);
                         alert("Friend Request Send")
-                      }
-                    
+                    }
+
                 })
                 .catch(error => {
                     // handle error
@@ -160,81 +160,90 @@ function Feed(props) {
 
     const delReq = async (id) => {
         axios.delete(`http://localhost:4000/friendrequest/${id}`)
-        .then(response => {
-          // handle success
-          console.log(response.data);
-          fetchFriendReq()
-        })
-        .catch(error => {
-          // handle error
-          console.log(error);
-        });
+            .then(response => {
+                // handle success
+                console.log(response.data);
+                fetchFriendReq()
+            })
+            .catch(error => {
+                // handle error
+                console.log(error);
+            });
     }
 
 
     return (
         <>
-         <Navbar />
+            <Navbar />
             <div className="container-fluid">
                 <div className="row my-3">
                     <div className="col-12 col-md-6 col-xl-3">
-                        <div className="text-center bg-light-subtle shadow p-3 py-4">
-                            <img src={require("../uploads/" + user.image)} className="rounded-circle" width="150" alt="" />
-                            <h2 className="fw-normal mb-0">{`${user.firstName} ${user.lastName}`}</h2>
-                            <h4 className="mb-3 text-muted" >{user.occupation}</h4>
-                            <p><a className="btn btn-secondary" >View details »</a></p>
+                        <div className="text-center bg-light-subtle shadow border-0 mb-3 py-4 card">
+                            <div className="card-body">
+                                <img src={require("../uploads/" + user.image)} className="rounded-circle" width="150" alt="" />
+                                <h2 className="fw-normal mb-0">{`${user.firstName} ${user.lastName}`}</h2>
+                                <h4 className="mb-3 text-muted" >{user.occupation}</h4>
+                                <p><a className="btn btn-secondary" >View details »</a></p>
+                            </div>
                         </div>
-                        <div>
-                            <div className="card">
-                                <div className="card-header">
-                                    Friends
+
+
+
+
+
+
+
+                      
+                            <div className="card shadow border-0">
+                                <div className="card-header border-0 bg-white pb-0 pt-3">
+                                    <h4> Friends</h4>
+                                   
                                 </div>
                                 <div className="card-body">
                                     {
-                                        
-                                        
-                                            friends.map((data) => (
 
-                                                <div className="card mb-3" key={data._id}>
-    
-                                                    <div className="p-2">
-                                                        <div className="d-flex justify-content-start">
-                                                            <div className="d-flex align-items-center">
-                                                                <img src={require("../uploads/" + data.image)} className="rounded-circle" width="45" alt="" />
-                                                                <div>
-                                                                    <h5 className="mb-0 mx-2" >{`${data.firstName} ${data.lastName}`}</h5>
-                                                                    <span className="mb-0 mx-2" >{`${data.location}`}</span>
-                                                                </div>
+
+                                        friends.map((data) => (
+
+                                            <div className="card border-0 shadow-sm mb-3" key={data._id}>
+
+                                                <div className="p-2">
+                                                    <div className="d-flex justify-content-start">
+                                                        <div className="d-flex align-items-center">
+                                                            <img src={require("../uploads/" + data.image)} className="rounded-circle" width="45" alt="" />
+                                                            <div>
+                                                                <h5 className="mb-0 mx-2" >{`${data.firstName} ${data.lastName}`}</h5>
+                                                                <span className="mb-0 mx-2" >{`${data.location}`}</span>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            ))
-                                        
-                                       
-                                       
-                                    
-                                   
+                                            </div>
+                                        ))
+
+
+
+
+
                                     }
                                 </div>
                             </div>
 
-                        </div>
+                      
                     </div>
 
 
                     <div className="col-12 col-md-6 col-xl-6">
                         <div>
-                            <div className="card">
-                                <div className="card-header d-flex align-items-center ">
-                                    <img src={require("../uploads/" + user.image)} className="rounded-circle" width="45" alt="" />
-                                    <h5 className="mb-0 mx-2" >{`${user.firstName} ${user.lastName}`}</h5>
-                                </div>
+                            <div className="card bg-white border-0 shadow">
+                                
                                 <div className="card-body">
+                                    
                                     <form method="post" encType="multipart/form-data">
-                                        <div className="form-floating mb-3">
-                                            <textarea className="form-control" placeholder="Leave a comment here" name="description" value={description} onChange={(e) => setDesc(e.target.value)} id="floatingTextarea2" style={{ height: "80px" }}></textarea>
-                                            <label htmlFor="floatingTextarea2">Description</label>
+                                        <div className=" d-flex">
+                                        <img src={require("../uploads/" + user.image)} className="rounded-circle" width="45" height="45" alt="" />
+                                            <textarea className="form-control border-0 shadow-none pt-2" placeholder={`What's on your mind, ${user.firstName} ${user.lastName}`} name="description" value={description} onChange={(e) => setDesc(e.target.value)} id="floatingTextarea2" ></textarea>
+                                            {/* <label htmlFor="floatingTextarea2">Description</label> */}
                                         </div>
                                         <div className="mb-3">
                                             <label htmlFor="formFile" className="form-label">Add Image</label>
@@ -251,21 +260,21 @@ function Feed(props) {
 
 
 
-                                    <div className="card my-3" key={post._id} >
+                                    <div className="card my-3 bg-white border-0 shadow" key={post._id} >
 
-                                        <div className="card-header d-flex align-items-center justify-content-between">
+                                        <div className="card-header d-flex align-items-center justify-content-between bg-white border-0">
                                             <div className="d-flex align-items-center">
                                                 <img src={require("../uploads/" + post.userImage)} className="rounded-circle" width="45" alt="" />
                                                 <div>
                                                     <h5 className="mb-0 mx-2" >{`${post.firstName} ${post.lastName}`}</h5>
-                                                    <span className="mb-0 mx-2" >{`${post.location}`}</span>
+                                                    <span className="mb-0 mx-2 text-muted" >{`${post.location}`}</span>
                                                 </div>
                                             </div>
                                             <button type="button" className="btn btn-primary btn-sm" onClick={() => sendFriendReq(post.userId)}>Add Friend</button>
                                         </div>
                                         <div className="card-body">
                                             <p className="card-text">{post.description}</p>
-                                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                            {/* <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
                                         </div>
                                         <img src={require("../uploads/" + post.image)} className="card-img-bottom" />
                                     </div>
@@ -278,14 +287,14 @@ function Feed(props) {
 
                     <div className="col-12 col-xl-3">
                         <div>
-                            <div className="card">
-                                <div className="card-header">
+                            <div className="card bg-white border-0 shadow">
+                                <div className="card-header bg-white border-0">
                                     Friend requests
                                 </div>
                                 <div className="card-body">
                                     {
                                         friendrequest.map((data) => (
-                                            
+
                                             <div className="card mb-3" key={data.request._id}>
 
                                                 <div className="p-2">
@@ -302,7 +311,7 @@ function Feed(props) {
                                                         <div className="card-body">
 
                                                             <button type="button" className="btn btn-primary btn-sm ms-5" onClick={() => confirmReq(data.request._id)} >Confirm</button>
-                                                            <button type="button" className="btn btn-outline-danger btn-sm ms-2"  onClick={()=> delReq(data.request._id)} >Delete</button>
+                                                            <button type="button" className="btn btn-outline-danger btn-sm ms-2" onClick={() => delReq(data.request._id)} >Delete</button>
 
                                                         </div>
                                                     </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,40 +1,29 @@
 
 import { NavLink } from 'react-router-dom'
-import {useAuth} from '../context/AuthProvider'
+import { useAuth } from '../context/AuthProvider'
 import React, { useState, useEffect } from 'react'
 
 function Navbar() {
 
   const authLocal = useAuth()
 
-  const [stickyClass, setStickyClass] = useState('');
 
-  useEffect(() => {
-    window.addEventListener('scroll', stickNavbar);
-    return () => window.removeEventListener('scroll', stickNavbar);
-  }, []);
-
-  const stickNavbar = () => {
-    if (window !== undefined) {
-      let windowHeight = window.scrollY;
-      
-      windowHeight > 150 ? setStickyClass('active') : setStickyClass('');
-    }
-  };
+  const user = JSON.parse(localStorage.getItem("user"));
 
 
-    return (
+  return (
 
-      <>
+    <>
 
 
-<nav className={`navbar navbar-expand-lg fixed-top py-5 ${stickyClass}`}>
+      {/* <nav className={`navbar navbar-expand-lg  py-3`}>
   <div className="container-fluid">
     
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse justify-content-end pe-nav" id="navbarSupportedContent">
+    <div className="collapse navbar-collapse justify-content-between " id="navbarSupportedContent">
+
       <ul className="navbar-nav mb-2 mb-lg-0 ">
         <li className="nav-item mx-3">
         <NavLink className="nav-link fs-5" to='/' >
@@ -70,15 +59,43 @@ function Navbar() {
       </ul>
     </div>
   </div>
-</nav>
-      
+</nav> */}
+
+
+      <header class="py-3 mb-4 border-bottom bg-white">
+        <div class="container d-flex flex-wrap justify-content-between">
+
+          <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
+            <input type="search" class="form-control" placeholder="Search..." aria-label="Search" />
+          </form>
+          <a href="/#" class="d-flex align-items-center mb-3 mb-lg-0 text-dark text-decoration-none">
+            <div class="dropdown text-end">
+              <a class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src={require("../uploads/" + user.image)} alt="mdo" width="32" height="32" class="rounded-circle" />
+              </a>
+              <ul class="dropdown-menu text-small" >
+
+                <li><a class="dropdown-item" >Profile</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                {authLocal.user && (<>
+                  <li className="dropdown-item">
+
+                    <button onClick={() => { authLocal.logout() }}>Logout</button>
+
+
+                  </li>
+                </>)}
+              </ul>
+            </div>
+
+          </a>
+        </div>
+      </header>
 
 
 
 
-
-
-       {/* <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+      {/* <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
         <div class="container-fluid">
           
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -121,10 +138,9 @@ function Navbar() {
           </div>
         </div>
       </nav>  */}
-  
-      </>
-    )
-  }
-  
-  export default Navbar
-  
+
+    </>
+  )
+}
+
+export default Navbar
